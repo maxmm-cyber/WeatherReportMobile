449078cd74842a292f1dd183e70a3fbf4df4b264
maxmm-cyber <maxm993993@gmail.com>
2024-09-12 20:36:52 +0300

    add Bar

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
 
 
-class MainApp(MDApp):
+class WScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class   WReportApp(MDApp):
     def build(self):
+        Builder.load_file("style.kv")
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Cyan"
 
-        return (
-            MDScreen(
-                MDRectangleFlatButton(
-                    text="Hello, World",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-            )
-        )
+        return WScreen()
+        
 
-
-MainApp().run()
+WReportApp().run()
