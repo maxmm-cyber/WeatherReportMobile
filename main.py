46a2d01f801179ef6a5bd4a486be3f5c1c71aaa0
maxmm-cyber <maxm993993@gmail.com>
2024-09-26 20:32:48 +0300

    lesson 3

--- main.py
+++ main.py
@@ -2,12 +2,29 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
+import requests
+from settings import*
 
 
 class WScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def get_weather(self,city):
+        params = {
+            "q": city,
+            "appid": API_KEY,
+        }
+        data = requests.get(CURRENT_WEATHER_URL,params)
+        response = data.json()
+        print(response)
+        return response
+    def search(self):
+        city = self.ids.city.text
+        weather = self.get_weather(city)
+
+        temp =  weather["main"]["temp"]
+        self.ids.temp.text = f"{round(temp)}°C"
 
 class   WReportApp(MDApp):
     def build(self):
