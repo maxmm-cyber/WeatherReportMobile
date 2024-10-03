c567bb857512515fdba27184a2fe81d70718ff11
maxmm-cyber <maxm993993@gmail.com>
2024-10-03 20:27:25 +0300

    lesson 4

--- main.py
+++ main.py
@@ -24,21 +24,32 @@ class WScreen(MDScreen):
         weather = self.get_weather(city)
 
         temp =  weather["main"]["temp"]
-        self.ids.temp.text = f"{round(temp)}°C"
+        self.ids.temp.text = f"Температура:{round(temp)}°C"
 
-        
-        
+
+        humidity =  weather["main"]["humidity"]
+        self.ids.humidity.text = f"Вологість:{round(humidity)}%"
+
+
 
         descr =  weather["weather"][0]["description"]
         self.ids.descr.text = f"{descr}"
 
+        feels_like =  weather["main"]["feels_like"]
+        self.ids.feel.text = f"Відчувається як:{round(feels_like)}°C"
+
+        wind =  weather["wind"]["speed"]
+        self.ids.wind.text = f"Вітер:{wind} м/с"     
+
+        icon =  weather["weather"][0]["icon"]
+        self.ids.icon.source = f'https://openweathermap.org/img/wn/{icon}@2x.png'
 
 
 class   WReportApp(MDApp):
     def build(self):
         Builder.load_file("style.kv")
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Cyan"
+        self.theme_cls.primary_palette = "Orange"
 
         return WScreen()
         
