1f87ac4bdee7b03b104cc41363a4b70640406048
maxmm-cyber <maxm993993@gmail.com>
2024-10-10 20:32:13 +0300

    lesson 5

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
 import requests
+from kivymd.uix.screenmanager import *
 from settings import*
 
 
@@ -44,14 +45,33 @@ class WScreen(MDScreen):
         icon =  weather["weather"][0]["icon"]
         self.ids.icon.source = f'https://openweathermap.org/img/wn/{icon}@2x.png'
 
+    def show_forecast(self):
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'forecast'
+    
+
+
+
+
+class FScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def back(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = 'home'
+
 
 class   WReportApp(MDApp):
     def build(self):
         Builder.load_file("style.kv")
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Orange"
-
-        return WScreen()
+        sm = MDScreenManager()
+        self.weather_screen = WScreen(name = "home")
+        self.forecast_screen = FScreen(name = "forecast")
+        sm.add_widget(self.weather_screen)
+        sm.add_widget(self.forecast_screen)
+        return sm
         
 
 WReportApp().run()
